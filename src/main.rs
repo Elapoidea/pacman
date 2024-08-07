f2f3b5f50922b2b39660868eb7eb947eeac83c10
Elapoidea <71977382+Elapoidea@users.noreply.github.com>
2024-08-07 11:12:13 -0500

    Cleaned up main file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use board::{Board, BitBoard, MoveType};
 mod piece;
 use piece::Piece;
 
+mod utils;
+use utils::fen;
+
 use rand::Rng;
 
 fn main() {
@@ -24,7 +27,6 @@ fn main() {
 
     // println!("f\n{}", board.moves(MoveType::Moves));
     // println!("{}", board);
-    
 
     for i in 0..100000 {
         let mut pawns: BitBoard = BitBoard(0);
@@ -35,46 +37,22 @@ fn main() {
     
         let mut board: Board = Board::init(piece, pawns);
 
-        // println!("Before:\n{}", board);
-
         board.create_path(20);
 
-        // println!("After:\n{}", board);
+        let s = board.attempt_solution();
 
-        if board.pawns.0 == 0 {
-            println!("{}", board.moves(MoveType::Moves));
+        if s {
+            println!("This has exactly one solution: \n{:?}\n{}\n{}", 
+            board.get_path().into_iter().rev().map(
+                |x| 
+                (["a", "b", "c", "d", "e", "f", "g", "h"][8-(1 + x as usize % 8)].to_owned() + 
+                ["1", "2", "3", "4", "5", "6", "7", "8"][x as usize / 8])
+            ).collect::<Vec<String>>(),
+            board, 
+            i
+        );
+            println!("{} {}", board.pawns.0, board.piece.square);
             break;
         }
     }
-
-
-    // for i in 0..100000 {
-    //     let mut pawns: BitBoard = BitBoard(0);
-    //     let piece: Piece = Piece::init(2, rng.gen_range(0..63));
-    //     // let piece: Piece = Piece::init(2, 14);
-    
-    //     pawns = !piece.location & pawns;
-    
-    //     let mut board: Board = Board::init(piece, pawns);
-
-    //     board.create_path(20);
-
-    //     println!("{}", board);
-
-    //     let s = board.attempt_solution();
-
-    //     if s {
-    //         println!("This has exactly one solution: \n{:?}\n{}\n{}", 
-    //         board.get_path().into_iter().rev().map(
-    //             |x| 
-    //             (["a", "b", "c", "d", "e", "f", "g", "h"][8-(1 + x as usize % 8)].to_owned() + 
-    //             ["1", "2", "3", "4", "5", "6", "7", "8"][x as usize / 8])
-    //         ).collect::<Vec<String>>(),
-    //         board, 
-    //         i
-    //     );
-    //         println!("{} {}", board.pawns.0, board.piece.square);
-    //         break;
-    //     }
-    // }
 }
---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+use crate::Board;
+
+pub fn fen (position: Board) -> String {
+    // 1p1p1p1p/p1p1p1p1/3p1p1p/p5p1/1p6/B1p5/1p1p1p1p/p7 w - - 0 1
+
+
+    "".to_string()
+}
